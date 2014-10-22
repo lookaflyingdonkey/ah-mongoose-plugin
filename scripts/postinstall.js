3a7214adab729b6f5336b0ccfe3f1ed03cd5afd3
Dean Collins <lookaflyingdonkey@icloud.com>
2014-10-22 14:54:28 +1100

    Fix up path in post install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,7 +7,7 @@ var mkdirp = require('mkdirp');
 var srcFile   = path.normalize(__dirname + '/../config/mongoose.js');
 var destFile = path.normalize(process.cwd() + '/../../config/mongoose.js');
 
-if(!fs.existsSync(projectFile)) {
+if(!fs.existsSync(destFile)) {
   console.log("Copying " + srcFile + " to " + destFile);
   fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
 }
